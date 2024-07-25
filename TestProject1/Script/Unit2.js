6af98e249a882460b3d3de22653b68e932e29710
unknown <pooja@lexingtonsoft.com>
2024-07-25 16:50:37 +0530

    first commit

---
+++ TestProject1/Script/Unit2.js
@@ -0,0 +1,14 @@
+﻿//JavaScript
+function test2()
+{
+var url = "https://smartbear.com";
+
+Browsers.Item(btChrome).Run(url);
+var page = Sys.Browser().Page(url);
+//var element = page.FindChildByXPath("//*[@id="drift-frame-chat"]");
+var style = page.contentDocument.defaultView.getComputedStyle(element, "");
+//Log.Message("The Properties of web page element are as follows")
+//Log.Message("Background Color : " + style.backgroundColor);
+//Log.Message("Font Family : " + style.fontFamily);
+//Log.Message("Font Size : " + style.fontSize);
+}
